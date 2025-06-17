a61f8766eb2a6215bbc0bce1dc6bc63f6144809e
mohammedriza-rahman <153982126+mohammedriza-rahman@users.noreply.github.com>
2025-06-17 11:35:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def main():
         ## display of the prediction
 
         if marks > 90:
-            st.ballons()
+            st.baloons()
             st.success(f"Congrats {name}! You are performing excellently with a score of {marks}.")
         elif marks >35:
             st.warning(f"{name}! You are performing well with a score of {marks}.")
